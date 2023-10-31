d8284f20312139e40adbdd9f1c91d20018e507d3
PekiPho <v.a.r.d.games123@gmail.com>
2023-10-31 21:20:04 +0100

    added all buttons to js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+const zero=document.getElementById('zero');
 const one=document.getElementById('one');
 const two=document.getElementById('two');
 const three=document.getElementById('three');
@@ -7,3 +8,15 @@ const six=document.getElementById('six');
 const seven=document.getElementById('seven');
 const eight=document.getElementById('eight');
 const nine=document.getElementById('nine');
+
+const point=document.getElementById('point');
+const equal=document.getElementById('equal');
+const clear=document.getElementById('clear');
+const back=document.getElementById('back');
+
+const plus=document.getElementById('plus');
+const minus=document.getElementById('minus');
+const divide=document.getElementById('divide');
+const multiply=document.getElementById('multiply');
+
+const display=document.querySelector('.display');
